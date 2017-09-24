153631b75f235b61157aaa15c8787e68b34583a7
Arista Soluciones <desarrollo@aristasoluciones.com>
2017-09-24 11:56:15 -0500

    reparando .gitignore

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('America/Mexico_City');
-define('WEB_ROOT','http://'.$_SERVER["HTTP_HOST"].'/sie');
-define('NUM_PAGINATION',100);
-$conexion=mysql_connect("localhost","root","");
-mysql_select_db("dbsie",$conexion);
-?> 
